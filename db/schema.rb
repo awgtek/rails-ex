f181a08b826f0b853c631ec36b72daf17cc267a5
awgtek <adamwg@hotmail.com>
2017-12-30 17:22:29 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,21 @@ ActiveRecord::Schema.define(version: 20161103113002) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
     t.text     "body"
     t.integer  "article_id"
+    t.integer  "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["category_id"], name: "index_comments_on_category_id"
   end
 
 end
