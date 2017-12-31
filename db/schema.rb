59c673d2206ae391c8edd8d70d54e52122468da0
awgtek <adamwg@hotmail.com>
2017-12-31 14:11:16 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -19,12 +19,6 @@ ActiveRecord::Schema.define(version: 20161103113002) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
     t.text     "body"
